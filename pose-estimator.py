b46cb8b764366d24665093f285b55feba723a19e
Ankit <ankit.gupta@artorg.unibe.ch>
2018-11-25 10:36:54 +0100

    added best pose estimation

--- pose-estimator.py
+++ pose-estimator.py
@@ -6,6 +6,8 @@ import itertools
 import numpy as np
 from scipy.spatial import distance
 import matplotlib.pyplot as plt
+from scipy.signal import argrelextrema
+import copy
 
 
 argparser = argparse.ArgumentParser(
@@ -35,16 +37,18 @@ def _main_(args):
     observed_points_path = args.observed_points
     known_points = read_points_location(known_points_path)
     observed_points = read_points_location(observed_points_path)
-    pose = PointsMatcher(known_points, observed_points)
+    potential_match = PointsMatcher(known_points, observed_points)
+    potential_matches = potential_match.get_potential_correspondences()
+    pose = PoseEstimator(known_points, observed_points, potential_matches)
+
 
 class PointsMatcher:
     def __init__(self, known_points, observed_points):
         self.known_points = known_points
         self.observed_points = observed_points
-        self.dmat = []
-        self.amat = []
-        corres_mat = self.generate_correspondence_matrix(known_points, observed_points)
-        poten_matches = self.get_potential_correspondences(corres_mat)
+        # self.dmat = []
+        # self.amat = []
+        self.correspondence_matrix = self.generate_correspondence_matrix(known_points, observed_points)
 
     def generate_correspondence_matrix(self, known_points, observed_points):
         known_triangle_idx, known_triangle_sides, known_areas = self.triangle_list_idx(known_points)
@@ -64,8 +68,14 @@ class PointsMatcher:
         triangle_lists_idx = []
         triangle_sides = []
         triangle_areas = []
+        points = np.asarray(points)
+        eps = 1e-3
         for subset in itertools.combinations(range(len(points)), 3):
             # TODO: add co-linearity condition
+            cross_prod = np.linalg.norm(np.cross(points[subset[0]]-points[subset[1]],
+                                  points[subset[0]]-points[subset[2]]))
+            if cross_prod < eps:
+                pass
             triangle_side_lengths = [distance_mat[subset[0], subset[1]], distance_mat[subset[1], subset[2]],
                                      distance_mat[subset[0], subset[2]]]
             # TODO: add for isoceles case
@@ -87,29 +97,200 @@ class PointsMatcher:
     def shape_parameter(self, triangle_sides_1, triangle_sides_2):
         d = (triangle_sides_1[0]-triangle_sides_2[0])**2 + (triangle_sides_1[0]-triangle_sides_2[0])**2 \
             + (triangle_sides_1[0]-triangle_sides_2[0])**2
-        self.dmat.append(d)
+        # self.dmat.append(d)
         alpha_l = 1
         shape_param = np.exp(-d/alpha_l)
         return shape_param
 
     def surface_parameter(self, triangle_area_1, triangle_area_2):
-        self.amat.append((triangle_area_1-triangle_area_2)**2)
+        # self.amat.append((triangle_area_1-triangle_area_2)**2)
         alpha_s = 10
         surface_param = np.exp(-(triangle_area_1-triangle_area_2)**2 / alpha_s)
         return surface_param
 
-    def get_potential_correspondences(self, correspondence_matrix):
-        matches_list = []
-        if correspondence_matrix.shape[0] <= correspondence_matrix.shape[1]:
-            matches = np.empty([correspondence_matrix.shape[0], 2], dtype=int)
-            for i in range(correspondence_matrix.shape[0]):
-                matches[i, :] = (i, np.argmax(correspondence_matrix[i, :]))
+    def get_potential_correspondences(self):
+        corres_mat = copy.deepcopy(self.correspondence_matrix)
+        potential_matches = []
+        search_times = 2
+        if corres_mat.shape[0] <= corres_mat.shape[1]:
+            while search_times > 0:
+                local_maximas = []
+                for i in range(corres_mat.shape[0]):
+                    potentialRightIdx = np.argmax(corres_mat[i, :])
+                    potentialLeftIdx = np.argmax(corres_mat[:, potentialRightIdx])
+                    if potentialLeftIdx == i:
+                        local_maximas.append([potentialLeftIdx, potentialRightIdx])
+                for i in range(len(local_maximas)):
+                    corres_mat[local_maximas[i][0], local_maximas[i][1]] = 0
+                search_times -= 1
+                potential_matches.append(local_maximas)
         else:
-            matches = np.empty([correspondence_matrix.shape[1], 2], dtype=int)
-            for i in range(correspondence_matrix.shape[1]):
-                matches[i, :] = (np.argmax(correspondence_matrix[:, i]), i)
+            while search_times > 0:
+                local_maximas = []
+                for i in range(corres_mat.shape[1]):
+                    potentialLeftIdx = np.argmax(corres_mat[:, i])
+                    potentialRightIdx = np.argmax(corres_mat[potentialLeftIdx, :])
+                    if potentialRightIdx == i:
+                        local_maximas.append([potentialLeftIdx, potentialRightIdx])
+                for i in range(len(local_maximas)):
+                    corres_mat[local_maximas[i][0], local_maximas[i][1]] = 0
+                search_times -= 1
+                potential_matches.append(local_maximas)
+
+        # search_times = 0
+        # if corres_mat.shape[0] >= corres_mat.shape[1]:
+        #     while search_times < 2:
+        #         local_maximas = np.empty([corres_mat.shape[1], 2])
+        #         for i in range(corres_mat.shape[1]):
+        #             local_maximas[i, :] = [np.argmax(corres_mat[:, i]), i]
+        #             corres_mat[np.argmax(corres_mat[:, i]), i] = 0
+        #         search_times += 1
+        #         potential_matches.append(local_maximas)
+        # else:
+        #     while search_times < 2:
+        #         local_maximas = np.empty([corres_mat.shape[0], 2])
+        #         for i in range(corres_mat.shape[0]):
+        #             local_maximas[i, :] = [i, np.argmax(corres_mat[i, :])]
+        #             corres_mat[i, np.argmax(corres_mat[i, :])] = 0
+        #         search_times += 1
+        #         potential_matches.append(local_maximas)
+
+        # if self.correspondence_matrix.shape[0] >= self.correspondence_matrix.shape[1]:
+        #     local_maximas = argrelextrema(self.correspondence_matrix, np.greater, mode='wrap', axis=0)
+        # else:
+        #     local_maximas = argrelextrema(self.correspondence_matrix, np.greater, mode='wrap', axis=1)
+        # values, start_idx, val_count = np.unique(local_maximas[0], return_index=True, return_counts=True)
+        # final_idx = []
+        # for i in range(len(val_count)):
+        #     if i == len(val_count)-1:
+        #         idx_values = [[local_maximas[0][j], local_maximas[1][j]] for j in range(start_idx[i], len(local_maximas[0]-1))
+        #                       if j < len(local_maximas[0])]
+        #     else:
+        #         idx_values = [[local_maximas[0][j], local_maximas[1][j]] for j in range(start_idx[i], start_idx[i+1])
+        #                       if j < len(local_maximas[0])]
+        #     if val_count[i] > 2:
+        #         # get best two similarity scores
+        #         similarity_score = []
+        #         for k in range(len(idx_values)):
+        #             similarity_score.append(self.correspondence_matrix[idx_values[k][0], idx_values[k][1]])
+        #         sorted_similarity_idx = np.argsort(similarity_score)
+        #         idx_values_new = [idx_values[sorted_similarity_idx[j]] for j in range(-2, 0)]
+        #     else:
+        #         idx_values_new = idx_values
+        #     final_idx.append(idx_values_new)
+        #
+        # potential_matches = np.asarray(potential_matches)
+
+        # # filter unique matches
+        # matches = [potential_matches[i, :, :] for i in range(potential_matches.shape[0]) if
+        #             len(np.unique(potential_matches[i, :, 1])) == len(potential_matches[i, :, 1])]
+
+        # generate subsets of matches
+        matches = []
+        for i in range(len(potential_matches)):
+            matches_subset = []
+            if len(potential_matches[i]) > 3:
+                for subset in itertools.combinations(range(len(potential_matches[i])), len(potential_matches[i])-1):
+                    for i in range(len(potential_matches[i])):
+                        match_subset = [potential_matches[i][j] for j in subset]
+                        matches_subset.append(match_subset)
+            elif len(potential_matches[i]) == 3:
+                matches_subset.append(potential_matches[i])
+        matches.extend(matches_subset)
         return matches
 
+
+class PoseEstimator:
+    def __init__(self, known_points, observed_points, potential_matches):
+        self.known_points = known_points
+        self.observed_points = observed_points
+        self.potential_matches = potential_matches
+        transforms = self.get_transform(known_points, observed_points, potential_matches)
+
+    def get_transform(self, known_points, observed_points, potential_matches):
+        errors = []
+        Ts = []
+        Rs = []
+        ts = []
+        for i in range(len(potential_matches)):
+            known_combination = np.empty([len(potential_matches[i]), 3])
+            observed_combination = np.empty([len(potential_matches[i]), 3])
+            for j in range(len(potential_matches[i])):
+                known_combination[j, :] = known_points[potential_matches[i][j][0]]
+                observed_combination[j, :] = observed_points[potential_matches[i][j][1]]
+            T, R, t = self.best_fit_transform(known_combination, observed_combination)
+            error = self.get_transformation_loss(known_combination, observed_combination, T)
+            errors.append(error)
+            Ts.append(T)
+            Rs.append(R)
+            ts.append(t)
+        min_error_idx = np.argmin(errors)
+        T_best = Ts[min_error_idx]
+        R_best = Rs[min_error_idx]
+        t_best = ts[min_error_idx]
+
+        return T_best, R_best, t_best
+
+    def best_fit_transform(self, A, B):
+        '''
+        Calculates the least-squares best-fit transform that maps corresponding points A to B in m spatial dimensions
+        Input:
+          A: Nxm numpy array of corresponding points
+          B: Nxm numpy array of corresponding points
+        Returns:
+          T: (m+1)x(m+1) homogeneous transformation matrix that maps A on to B
+          R: mxm rotation matrix
+          t: mx1 translation vector
+        '''
+        if A.shape != B.shape:
+            print('foo')
+
+        assert A.shape == B.shape
+
+        # get number of dimensions
+        m = A.shape[1]
+
+        # translate points to their centroids
+        centroid_A = np.mean(A, axis=0)
+        centroid_B = np.mean(B, axis=0)
+        AA = A - centroid_A
+        BB = B - centroid_B
+
+        # rotation matrix
+        H = np.dot(AA.T, BB)
+        U, S, Vt = np.linalg.svd(H)
+        R = np.dot(Vt.T, U.T)
+
+        # special reflection case
+        if np.linalg.det(R) < 0:
+            Vt[m - 1, :] *= -1
+            R = np.dot(Vt.T, U.T)
+
+        # translation
+        t = centroid_B.T - np.dot(R, centroid_A.T)
+
+        # homogeneous transformation
+        T = np.identity(m + 1)
+        T[:m, :m] = R
+        T[:m, m] = t
+
+        return T, R, t
+
+    def get_transformation_loss(self, A, B, transform):
+        m = A.shape[1]
+        # make points homogeneous, copy them to maintain the originals
+        src = np.ones((m + 1, A.shape[0]))
+        dst = np.ones((m + 1, B.shape[0]))
+        src[:m, :] = np.copy(A.T)
+        dst[:m, :] = np.copy(B.T)
+
+        src = np.dot(transform, src)
+
+        # error
+        distances = np.linalg.norm((src - dst), axis=0)
+        mean_error = np.mean(distances)
+
+        return mean_error
 
 if __name__ == '__main__':
     args = argparser.parse_args(['-kp', 'known_markers.csv', '-op', 'observed_markers.csv'])
